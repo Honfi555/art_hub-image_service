ac58e7002802960564d56c7061483e8d4d4a2426
kasan <kasanindaniil@gmail.com>
2025-04-19 12:23:43 +1000

    add environment variable loading for SECRET_KEY and ALGORITHM

--- app/static.py
+++ app/static.py
@@ -3,6 +3,8 @@ import os
 
 dotenv.load_dotenv(".env")
 
+SECRET_KEY: str = os.getenv("SECRET_KEY")
+ALGORITHM: str = os.getenv("ALGORITHM")
 REDIS_HOST: str = os.getenv("REDIS_HOST")
 REDIS_PORT: int = int(os.getenv("REDIS_PORT")) if isinstance(os.getenv("REDIS_PORT"), str) else 6379
 REDIS_DB: int = int(os.getenv("REDIS_DB")) if isinstance(os.getenv("REDIS_DB"), str) else 0
